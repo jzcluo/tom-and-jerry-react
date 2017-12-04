3564b94b161a0bfd6306351d14aa7e33fbce4075
JLuo <jeffreyzcluo@gmail.com>
2017-12-04 16:47:49 -0500

    added seperate onChangeListener in MapOption

--- src/components/map/MapOptionBar.jsx
+++ src/components/map/MapOptionBar.jsx
@@ -56,6 +56,7 @@ class MapOptionBar extends React.Component {
                 "Incident Address" : "",
                 "Incident Zip" : ""
             });
+            alert("Report Added");
             //increment counter
             database.ref('/').child("IDcounter").set({
                 counter : ID+1
@@ -63,9 +64,15 @@ class MapOptionBar extends React.Component {
         });
     };
     //for choosing dates
-    handleChange = (fieldname, date) => {
+    handleDateChange = (fieldname, date) => {
         let newState = {};
         newState[fieldname] = date;
+        this.setState(newState);
+    };
+
+    handleChange = (fieldname, event) => {
+        let newState = {};
+        newState[fieldname] = event.target.value;
         this.setState(newState);
     };
 
@@ -77,9 +84,9 @@ class MapOptionBar extends React.Component {
             case 'SEARCHBYDATE':
                 sidebar =   <div>
                                 <p>Start date:</p>
-                                <DatePicker className="sidebar-datepicker" selected={this.state.startDate} onChange={this.handleChange.bind(null, "startDate")}/>
+                                <DatePicker className="sidebar-datepicker" selected={this.state.startDate} onChange={this.handleDateChange.bind(null, "startDate")}/>
                                 <p>End date:</p>
-                                <DatePicker className="sidebar-datepicker" selected={this.state.endDate} onChange={this.handleChange.bind(null, "endDate")}/>
+                                <DatePicker className="sidebar-datepicker" selected={this.state.endDate} onChange={this.handleDateChange.bind(null, "endDate")}/>
                                 <button className='button' onClick={() => this.props.onDateButtonClick(this.state.startDate, this.state.endDate)}>Search By Date</button>
                                 <button className='button' onClick={() => this.props.onDisplayReportButtonClick()}>Display Report</button>
                                 <button className='button' onClick={() => this.props.onAddReportButtonClick()}>Add Report</button>
